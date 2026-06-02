7002d212696e1d043f70b91ea72dac49a812cdf1
AraujoAtila <atila.oliveira19@gmail.com>
2026-06-02 16:40:37 -0400

    adicionando url da vercel ao CORS

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,9 @@ app = FastAPI(title="BioFasting API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173,"
+                    "https://biofasting.vercel.app/"
+                   ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
